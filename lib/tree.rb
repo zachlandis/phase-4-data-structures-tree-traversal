0d18616f61d3678dc8342f035a481e8d8753581b
Zach <zachlandis91@gmail.com>
2023-09-01 13:36:08 -0600

    commit tree 5ef83c9ef2be70ec8642c7634c1c36a0fd1ef43d

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,16 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(id)
+    nodes_to_visit = [@root]
+
+    until nodes_to_visit.empty?
+      current = nodes_to_visit.pop
+      return current if current[:id] == id
+
+      nodes_to_visit = nodes_to_visit + current[:children]
+    end
+    nil
+  end
 end
